75f3972a63bd0d0119dca20deace1f646727a695
Юра <89320dota@gmail.com>
2018-04-30 21:38:53 +0300

    adding a logger

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,12 +1,17 @@
 package com.example;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	private static Logger logger = Logger.getLogger(DemoApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Starting SpringBootDemoApplication...");
 		SpringApplication.run(DemoApplication.class, args);
+		logger.info("Run SpringBootDemoApplication...");
 	}
 }
